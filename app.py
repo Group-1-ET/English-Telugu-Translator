357e33ef57dc92c2906be698b9f9dedac459f68c
komalreddy3 <koukuntlakomalreddy3@gmail.com>
2024-04-28 11:11:53 +0530

    final deploy

--- app.py
+++ app.py
@@ -3,28 +3,25 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        sentence = request.form['sentence']
+
+        if sentence:
+            # Construct the command
+            command = ["python3", "pages/model/inference.py", "--text", sentence, "-l", "te"]
+
+            # Run the command
+            process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            # Capture the output and error messages
+            output, error = process.communicate()
+
+            # Return the output directly
+            return output.decode()
+
     return render_template('index.html')
-
-@app.route('/result', methods=['POST'])
-def result():
-    sentence = request.form['sentence']
-
-    if sentence:
-        # Construct the command
-        command = ["python3", "pages/model/inference.py", "--text", sentence, "-l", "te"]
-        print(command)
-        print(sentence)
-        # Run the command
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        # Capture the output and error messages
-        output, error = process.communicate()
-        
-        return render_template('result.html', sentence=sentence, output=output.decode(), error=error.decode())
-
-    return "No sentence provided."
 
 if __name__ == '__main__':
     app.run(debug=True)
